c2f3f8ad08818cf1faf6072c1755007751a30d69
carolinaemanuele <carol.e.sa@hotmail.com>
2024-04-25 11:53:32 -0300

    Leet Code - Question 120

---
+++ Leet_Code-120/Leet_Code-120.java
@@ -0,0 +1,16 @@
+/*
+ * Question 120
+ */
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            List<Integer> row = triangle.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                int currentValue = row.get(j);
+                row.set(j, currentValue + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
